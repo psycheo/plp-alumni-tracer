a75c1c062a2318ba73c1ebc29f89215908085847
LANSYYY <lansadrianalonzo@gmail.com>
2026-03-17 21:44:51 +0800

    Update admin_models.php

--- admin/admin_models.php
+++ admin/admin_models.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-// Security check: Uncomment when ready
+//// Security check: Uncomment when ready
 // if (!isset($_SESSION['loggedin']) || $_SESSION['role'] !== 'admin') { header("Location: login.php"); exit; }
 ?>
 
