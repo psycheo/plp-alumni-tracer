579613697e3427ef49f182a6e39894181c496055
Kelly Rowland Lola <kellyrowland.lola530@gmail.com>
2026-04-10 20:47:35 +0800

    Sidebar Alignment

--- includes/admin_sidebar.php
+++ includes/admin_sidebar.php
@@ -52,7 +52,7 @@
         </li>
     </ul>
 
-    <div class="sidebar-footer">
+    <div class="sidebar-footer" style="padding-left: 0; padding-right: 0;">
         <ul class="sidebar-menu" style="padding: 0;">
             <li>
                 <a href="../index.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
